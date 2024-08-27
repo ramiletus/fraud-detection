05ab7312be46200589fcffea6c955d842c89adcf
ramiletus <juanramdiaz2a@gmail.com>
2024-08-27 17:19:18 +0200

    Added test case: given a single Operator is added more than once through different PhoneNumbers from Users when these last are injected, only one instance of that Operator exists in database.

--- src/test/java/com/ramiletus/frauddetection/service/users/UsersCommandHandlerTest.java
+++ src/test/java/com/ramiletus/frauddetection/service/users/UsersCommandHandlerTest.java
@@ -1,7 +1,9 @@
 package com.ramiletus.frauddetection.service.users;
 
 import com.ramiletus.frauddetection.FraudDetectionApplication;
+import com.ramiletus.frauddetection.persistence.dao.OperatorDao;
 import com.ramiletus.frauddetection.persistence.dao.UserDao;
+import com.ramiletus.frauddetection.persistence.model.Operator;
 import com.ramiletus.frauddetection.persistence.model.User;
 import com.ramiletus.frauddetection.service.users.injectusers.InjectUserCommand;
 import org.junit.jupiter.api.Test;
@@ -12,22 +14,26 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = FraudDetectionApplication.class)
-@Transactional
 class UsersCommandHandlerTest {
 
     @Autowired
     UserDao userDao;
 
     @Autowired
+    OperatorDao operatorDao;
+
+    @Autowired
     UsersCommandHandler usersCommandHandler;
 
     @Test
+    @Transactional
     void handleInjectUser() {
         // Given
         InjectUserCommand injectUserCommand = new InjectUserCommand();
@@ -52,6 +58,55 @@ class UsersCommandHandlerTest {
         assertEquals(1, foundUsers.size());
         assertEquals("John Doe", foundUsers.get(0).getName());
 
+    }
+
+
+    @Test
+    @Transactional
+    void onlyOneOperatorByNameIsCreated() {
+        // Given
+        InjectUserCommand injectUserCommand1 = new InjectUserCommand();
+        injectUserCommand1.setName("John Doe");
+        injectUserCommand1.setEmail("johndoe@udc.es");
+
+        PhoneNumberDTO phoneNumber1 = new PhoneNumberDTO();
+
+        phoneNumber1.setNumber(123456789L);
+        phoneNumber1.setIsMainNumber(true);
+        phoneNumber1.setOperator("Operator1");
+
+        PhoneNumberDTO phoneNumber2 = new PhoneNumberDTO();
+
+        phoneNumber1.setNumber(987654321L);
+        phoneNumber1.setIsMainNumber(false);
+        phoneNumber1.setOperator("Operator2");
+
+        injectUserCommand1.setPhoneNumbers(List.of(phoneNumber1, phoneNumber2));
+
+
+        InjectUserCommand injectUserCommand2 = new InjectUserCommand();
+        injectUserCommand2.setName("Don Joe");
+        injectUserCommand2.setEmail("donjoe@udc.es");
+
+
+        PhoneNumberDTO phoneNumber3 = new PhoneNumberDTO();
+        phoneNumber3.setNumber(666666666L);
+        phoneNumber3.setIsMainNumber(true);
+        phoneNumber3.setOperator("Operator1");
+
+        injectUserCommand2.setPhoneNumbers(List.of(phoneNumber3));
+
+        // When
+        usersCommandHandler.handleInjectUser(injectUserCommand1);
+        usersCommandHandler.handleInjectUser(injectUserCommand2);
+
+        // Then
+        Optional<Operator> foundOperator = operatorDao.findByName("Operator1");
+
+        assertAll(
+                () -> assertTrue(foundOperator.isPresent()),
+                () -> assertEquals("Operator1", foundOperator.get().getName())
+        );
     }
 
 }
